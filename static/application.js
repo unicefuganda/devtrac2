3d7ca40d9889590f558a811fd16ca2b56eefd6fc
Timothy Cochran <timothy.cochran@gmail.com>
2013-08-26 16:23:25 +0300

    add district data to open street maps [#55630730]

--- static/application.js
+++ static/application.js
@@ -1,7 +1,15 @@
-var map = L.map('map');
+$(function(){
+	$.getJSON("/static/districts.json", function(districts) {  
 
-var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © OpenStreetMap contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 7, maxZoom: 12, attribution: osmAttrib})
-map.setView(new L.LatLng(0.3136,32.5811),7);
-map.addLayer(osm);
+		var map = L.map('map');
+		var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+		var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 15})
+		map.setView(new L.LatLng(0.3136,32.5811),7);
+		map.addLayer(osm);
+		
+		L.geoJson(districts, {style:  { "color": "red", "weight": 1 } }).addTo(map);
+	});
+
+});
+
+
