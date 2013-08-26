977d89c214c8410198cbf78498ea2c498b5523fc
Timothy Cochran <timothy.cochran@gmail.com>
2013-08-26 10:29:12 +0300

    cleaning up spike [#55619188]

--- application.py
+++ application.py
@@ -2,14 +2,9 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route("/hello/<name>")
-@app.route("/hello/")
-def index(name=None):
-	return render_template('index.html', name=name)
-
-@app.route("/map/<location>")
-def map(location):
-    return "<h1>Map for %s</h1>" % location
+@app.route("/")
+def index():
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
