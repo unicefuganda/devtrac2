9d133c68722844c190c8bfba3a67be64afb3d6ef
Timothy Cochran <timothy.cochran@gmail.com>
2013-08-28 14:14:58 +0300

    added provisioning and bootstrap to fabric [#55630830]

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,13 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
 
+# env.user = 'root'
+# env.password = os.environ['DEVTRAC2_QA_PASSWORD']
+# env.hosts = ['192.237.187.62']
+
 env.user = 'root'
-env.password = os.environ['DEVTRAC2_QA_PASSWORD']
-env.hosts = ['192.237.187.62']
+env.password = os.environ['DEVTRAC2_UAT_PASSWORD']
+env.hosts = ['192.237.180.109']
 
 def deploy(char):
     print "deploying char #%s" % char
@@ -13,3 +17,14 @@ def deploy(char):
         run("git fetch origin")
         run("git reset --hard %s" % char)
         run("touch .wsgi")
+
+def bootstrap_chef():
+    run("curl -L https://get.rvm.io | bash")
+    run("source /etc/profile.d/rvm.sh")
+    run("rvm install 1.9.3")
+    run("gem install chef ruby-shadow --no-ri --no-rdoc")
+    run("sudo apt-get install git")
+    run("cd ~/; git clone https://github.com/unicefuganda/devtrac2-provisioning.git")
+
+def provision():
+    run("cd ~/devtrac2-provisioning; chef-solo -c solo.rb")
