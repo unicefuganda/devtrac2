af8647ce15d788bbf2ede6df85e05c4b732a1248
Opima Jude <ojude@thoughtworks.com>
2013-08-29 15:05:08 +0300

    staged education/schools visualization data for charting

--- static/javascript/education.js
+++ static/javascript/education.js
@@ -1,24 +1,19 @@
 var ckan = {};
 
-ckan.SearchURL = '/ugdata/';
-ckan.SearchTerm = 'school';
+ckan.get_data_url = '/download_resource';
 
 ckan.packageSearch = function(){
 
   this.start = function() {
 
    $.ajax({
-        url: ckan.SearchURL+ckan.SearchTerm,
+        url: ckan.get_data_url,
         type: "GET",
         data: {},
-        success: function(dataSet){
-          $("#resultMessage").append('<h3>'+dataSet.count+'  records marched search query</h3>');          
-          $.each(dataSet.results,function(index,data){
-
-            $("#packageList").append(constructTable(data));
-
-          });
-
+        success: function(dataSet){         
+         display_chart();
+         console.log(dataSet);
+         
         },
         error:function(){
             $("#errorMessage").html('Errors Encountered getting dataSet, try again later or contact your system admin');
@@ -29,7 +24,70 @@ ckan.packageSearch = function(){
 
 }
 
-  function constructTable(data){
+function display_chart(){
+  $('#chart_container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Historic World Population by Region'
+            },
+            subtitle: {
+                text: 'Source: Wikipedia.org'
+            },
+            xAxis: {
+                categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (millions)',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' millions'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 100,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: '#FFFFFF',
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Year 1800',
+                data: [107, 31, 635, 203, 2]
+            }, {
+                name: 'Year 1900',
+                data: [133, 156, 947, 408, 6]
+            }, {
+                name: 'Year 2008',
+                data: [973, 914, 4054, 732, 34]
+            }]
+        });
+}
+  function construct_table(data){
 
     var table = '<table style="border:1px solid #000000; width:90%;">';
     table += "<tr><td>Column<td><td>Value</td></tr>";
