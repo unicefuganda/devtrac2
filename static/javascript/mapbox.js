2a58009aaa3d0da01072b70454da143adb808dfb
Opima Jude <ojude@thoughtworks.com>
2013-08-30 18:27:29 +0300

    added district tile layer to mapbox spike [#56056234]

--- static/javascript/mapbox.js
+++ static/javascript/mapbox.js
@@ -3,4 +3,14 @@ var mapbox = {};
 
 $(function(){  
   var map = L.mapbox.map('map', 'jopima.map-qiqfxyek');
+
+  var uganda_disricts = L.tileLayer.wms("http://ec2-54-218-182-219.us-west-2.compute.amazonaws.com/geoserver/geonode/wms", {
+	    layers: 'geonode:uganda_districts_2010',
+	    format: 'image/png',
+	    transparent: true,
+	    attribution: "Uganda disctrict data"
+	});	
+	
+	map.addLayer(uganda_disricts);
+
 })
