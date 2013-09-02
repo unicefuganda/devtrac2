92b5009fc28a9c657f98b6b5a65acbf25bc3b830
Opima Jude <ojude@thoughtworks.com>
2013-09-02 18:43:51 +0300

    fixed add feature layer to map [#56203018]

--- static/javascript/polygons.js
+++ static/javascript/polygons.js
@@ -2,48 +2,46 @@
 var polygon = {};
 
 polygon.map = null;
-polygon.layer = null;
+polygon.Baselayer = null;
 polygon.polygonLayer = null;
 polygon.Control = null;
 
 polygon.init = function(){
+  polygon.map = new OpenLayers.Map( 'map');
+  polygon.Baselayer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+  layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", 
+                    "http://vmap0.tiles.osgeo.org/wms/vmap0",
+                    {layers: 'basic'} );
 
-this.start = function(){
-polygon.map = new OpenLayers.Map( 'map');
-polygon.polygonLayer = new OpenLayers.Layer.Vector("Polygon Layer");
-polygon.layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+  var tiled = polygon.tiles();
+  console.log(tiled);
 
-var tiles = polygon.tiles();
+   polygon.map.addLayers([tiled, layer]);
 
- polygon.map.addLayers([tiles, polygon.layer, polygon.polygonLayer]);
- polygon.map.addControl(new OpenLayers.Control.LayerSwitcher());
- polygon.map.addControl(new OpenLayers.Control.MousePosition());
+   var geojson_format = new OpenLayers.Format.GeoJSON();
+   var vector_layer = new OpenLayers.Layer.Vector(); 
 
- var geo_format = polygon.geoFormat();
- 
-  polygon.polygonLayer.addFeatures(geo_format.read(polygon.geoJSon()));
+   polygon.map.setCenter(new OpenLayers.LonLat(32.5811, 0.3136), 7); 
 
 
- polygon.map.setCenter(
-            new OpenLayers.LonLat(32.5811, 0.3136).transform(
-                new OpenLayers.Projection("EPSG:4326"),
-                polygon.map.getProjectionObject()
-            ), 7
-        );
-}
+   polygon.map.addLayer(vector_layer);
+   vector_layer.addFeatures(geojson_format.read(polygon.featureCollections()));
 
 }
 
 polygon.tiles = function(){
- format = 'image/png';
- tiles = new OpenLayers.Layer.WMS(
+
+var map = polygon.map;
+var format = 'image/png';
+
+  var tiles = new OpenLayers.Layer.WMS(
             "geonode:uganda_districts_2010 - Tiled", "http://ec2-54-218-182-219.us-west-2.compute.amazonaws.com/geoserver/geonode/wms",
             {
                 LAYERS: 'geonode:uganda_districts_2010',
                 STYLES: '',
                 format: format,
                 tiled: true,
-                tilesOrigin : polygon.map.maxExtent.left + ',' + polygon.map.maxExtent.bottom
+                tilesOrigin : map.maxExtent.left + ',' + map.maxExtent.bottom
             },
             {
                 buffer: 0,
@@ -53,69 +51,51 @@ polygon.tiles = function(){
             } 
         );
 
-return tiles;
+  return tiles;
 }
 
-polygon.geoFormat = function(){
 
-var map = polygon.map;
+polygon.featureCollections = function(){
+ var featurecollection = {
+              "type": "FeatureCollection", 
+              "features": [
+                {"geometry": {
+                    "type": "GeometryCollection", 
+                    "geometries": [
+                        {
+                            "type": "LineString", 
+                            "coordinates": 
+                                [[32.5830574,0.337122083], 
+                                [32.4055481,0.463387102]]
+                        }, 
+                        {
+                            "type": "Polygon", 
+                            "coordinates": 
+                                [[
+[31.73174667,3.231184721],
+[32.78620529,1.639039993],
+[32.75101471,2.301280737],
+[30.89932728,3.041703939],
+[30.96674156,2.840301633]
+]]
+                        },
+                        {
+                            "type":"Point", 
+                            "coordinates":[31.60180569,-0.367727906]
+                        }
+                    ]
+                }, 
+                "type": "Feature", 
+                "properties": {}}
+              ]
+           };
+           return featurecollection;
 
-	var geojson_format = new OpenLayers.Format.GeoJSON({
-                'internalProjection': map.baseLayer.projection,
-                'externalProjection': new OpenLayers.Projection("EPSG:4326")
-            });
-
-	return geojson_format;
-}
-polygon.geoJSon = function(){
-
- var geoJSON = { "type": "FeatureCollection",
-                "features": 
-                [
-
-                    { "type": "Feature", "properties": { "LENGTH": 756.304000},
-                     "geometry": { "type": "LineString", 
-                     "coordinates": [ 
-                     [ 18.105018, 59.231027 ], [ 18.104176, 59.230737 ],
-                      [ 18.103928, 59.230415 ], [ 18.103650, 59.230336 ], 
-                      [ 18.103028, 59.230463 ], [ 18.102491, 59.230418 ], 
-                      [ 18.101976, 59.230237 ], [ 18.100893, 59.230110 ], 
-                      [ 18.100117, 59.230016 ], [ 18.097715, 59.230262 ], 
-                      [ 18.096907, 59.230376 ], [ 18.096637, 59.230405 ],
-                       [ 18.096578, 59.230428 ], [ 18.096429, 59.230450 ],
-                        [ 18.096336, 59.230479 ], [ 18.096108, 59.230534 ], 
-                        [ 18.095971, 59.230600 ], [ 18.095925, 59.230633 ], 
-                        [ 18.095891, 59.230665 ], [ 18.094000, 59.231676 ], 
-                        [ 18.093864, 59.231720 ] ] } }
-                    ,                   
-            { "type": "Feature", "properties": { "LENGTH": 1462.390000},
-             "geometry": { "type": "LineString", 
-             "coordinates": [ 
-             [ 17.877073, 59.461653 ], [ 17.877116, 59.461598 ], 
-             [ 17.876936, 59.461507 ], [ 17.876936, 59.461323 ],
-              [ 17.876773, 59.461098 ], [ 17.876430, 59.460885 ], 
-              [ 17.876413, 59.460553 ], [ 17.876576, 59.460280 ], 
-              [ 17.876575, 59.460078 ], [ 17.876762, 59.460060 ], 
-              [ 17.877371, 59.460042 ], [ 17.877808, 59.460046 ], 
-              [ 17.878641, 59.460046 ], [ 17.879010, 59.460078 ], 
-              [ 17.879337, 59.460044 ], [ 17.879526, 59.459878 ], 
-              [ 17.879749, 59.459563 ], [ 17.880058, 59.459538 ], 
-              [ 17.880435, 59.459503 ], [ 17.887550, 59.453608 ], 
-              [ 17.887696, 59.453430 ], [ 17.887971, 59.453150 ], 
-              [ 17.888221, 59.452843 ], [ 17.888246, 59.452721 ], 
-              [ 17.888435, 59.452609 ], [ 17.888470, 59.452568 ], 
-              [ 17.888517, 59.452410 ] ] } }
-
-                ]
-            };
-
-return geoJSON;
 }
 
 
 $(function(){
 
-var load_map = new polygon.init();
-load_map.start();
+polygon.init();
 
 });
