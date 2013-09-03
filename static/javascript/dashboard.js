f890a7e45190031ec50273c93b291854ba0ba0d4
Timothy Cochran <timothy.cochran@gmail.com>
2013-09-03 13:14:11 +0300

    introducing district service [#56109594]

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,4 +1,4 @@
-angular.module('dashboard', []).
+var dashboard = angular.module('dashboard', []).
   config(function($routeProvider, $interpolateProvider, $locationProvider) {  
     $locationProvider.html5Mode(true);    
     $routeProvider
@@ -10,18 +10,39 @@ angular.module('dashboard', []).
     $interpolateProvider.endSymbol('}]}');
   });
 
+var myApp = dashboard.service('districtService', function($http, $filter) {
+  var self = this;
+  this.all = function(result) {
+    if (self.districts) {
+      result(self.districts);
+      return;
+    }
 
-function ListCtrl($scope, $http) {
-  $http({method: 'GET', url: '/districts.json'}).
-    success(function(data, status, headers, config) { $scope.districts = data.districts });
+    $http({method: 'GET', url: '/districts.json'}).
+      success(function(data, status, headers, config) {  
+        self.districts = data.districts;
+        result(data.districts); 
+      });
+  };
+
+  this.find_by_name = function(name, result) {
+    self.all(function(districts) {
+      var district = $filter('filter')(districts, function(district) { return district.name.toLowerCase() == name.toLowerCase(); })[0]; 
+      result(district);
+    });
+  };
+});
+
+function ListCtrl($scope, $http, districtService) {
+  districtService.all(function(districts){
+    $scope.districts = districts;
+  });
 }
 
 function SpikesCtrl() {}
 
-function ShowCtrl($scope, $http, $filter, $routeParams) {
-  console.log("Show!")
-  $http({method: 'GET', url: '/districts.json'}).
-    success(function(data, status, headers, config) { 
-      $scope.district = $filter('filter')(data.districts, function(district) { return district.name.toLowerCase() == $routeParams.district.toLowerCase(); })[0]
-    });
+function ShowCtrl($scope, $http, $filter, $routeParams, districtService) {
+  districtService.find_by_name($routeParams.district, function(district){
+    $scope.district = district;
+  });
 }
