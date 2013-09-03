37d0f8f98502e353ac6c26c4c2b39995bc4d788d
Timothy Cochran <timothy.cochran@gmail.com>
2013-09-03 13:40:13 +0300

    fixing tests

--- features/dashboard_steps.py
+++ features/dashboard_steps.py
@@ -6,7 +6,7 @@ from nose.tools import assert_equals
 @step(u'When I open dashboard for (\w+)')
 def when_i_open_dashboard_for_(step, district):
     world.page = Page(Browser('phantomjs'))
-    world.page.visit("http://localhost:5000/district/%s" % district)
+    world.page.visit("http://localhost:5000/#/district/%s" % district)
 
 @step(u'Then I see (.+) as the title')
 def then_i_see_kamapala_title(step, title):
