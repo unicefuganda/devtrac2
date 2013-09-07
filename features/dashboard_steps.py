964bd6c74fc5552a206396a1e3b5df246aed78ea
Timothy Cochran <timothy.cochran@gmail.com>
2013-09-07 21:13:05 +0530

    not using cdn, too slow in uganda, organizing css and js into lib folder [#56493186]

--- features/dashboard_steps.py
+++ features/dashboard_steps.py
@@ -5,7 +5,7 @@ from time import *
 
 @step(u'When I open dashboard for (\w+)')
 def when_i_open_dashboard_for_(step, district):
-    world.page = Page("phantomjs")
+    world.page = Page(world.browser)
     world.page.visit("http://localhost:5000/#/district/%s" % district)
     sleep(0.5)
     
--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,3 +1,27 @@
+var map;
+
+function loadLeafletMap(lng, lat, zoom) {  
+  map.setView(new L.LatLng(lat, lng), zoom);
+}
+
+$(function(){
+  map = L.map('map');
+  var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttrib='Map data © OpenStreetMap contributors';
+  var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 15, attribution: osmAttrib});   
+  
+  var uganda_districts = L.tileLayer.wms("http://ec2-54-218-182-219.us-west-2.compute.amazonaws.com/geoserver/geonode/wms", {
+      layers: 'geonode:uganda_districts_2010',
+      format: 'image/png',
+      transparent: true,
+      attribution: "Uganda disctrict data"
+  });
+
+  map.addLayer(osm);
+  map.addLayer(uganda_districts);
+
+});
+
 var dashboard = angular.module('dashboard', []).
   config(function($routeProvider, $interpolateProvider, $locationProvider) {  
     // $locationProvider.html5Mode(true);    
