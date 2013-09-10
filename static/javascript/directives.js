7a1472f8de743ac6b74498ecb59097a19d9511e5
Timothy Cochran <timothy.cochran@gmail.com>
2013-09-10 17:28:29 +0300

    display districts layer [Finishes #56495412]

--- static/javascript/directives.js
+++ static/javascript/directives.js
@@ -3,7 +3,7 @@ angular.module("dashboard").directive('map', function() {
       link: function(scope, element, attrs) {        
         var map = new DevTrac.Map(element);
         window.map = map;
-        var simplified_layers = ["01", "005", "002", "0015", "001", "0005", "0002" ];
+        var simplified_layers = ["01", "005", "0015", "001", "0005", "0002" ];
         $.each(simplified_layers, function(index, tolerence) {
           // this need to move to a controller
           $.getJSON("/static/javascript/geojson/uganda_districts_2011_" + tolerence + ".json", function(geojsonFeature, textStatus, jqXHR) {
