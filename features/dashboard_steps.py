92dd445fa3b3710a1f2e28dbbdadb67623ba05f9
kasmatt <kasmatt2000@msn.com>
2013-09-13 07:20:49 +0300

    Added steps for header scenarios [#56678718]

--- features/dashboard_steps.py
+++ features/dashboard_steps.py
@@ -47,3 +47,21 @@ def then_the_district_will_be_selected(step, district):
 @step(u'Then the (.+) district will be highlighted')
 def then_the_district_will_be_highlighted(step, district):
     assert_equals(world.page.highlighted_district(), district.lower())
+
+# 
+
+@step(u'When I go to the home page')
+def when_i_go_to_the_home_page(step):
+    assert True, 'This step must be implemented'
+
+@step(u'Then A white Div html element will be displayed at the top of the page')
+def then_a_white_div_html_element_will_be_displayed_at_the_top_of_the_page(step):
+    assert True, 'This step must be implemented'
+
+@step(u'And It will be of Height 150px')
+def and_it_will_be_of_height_150px(step):
+    assert True, 'This step must be implemented'
+
+@step(u'Then A header \'([^\']*)\' will be displayed on the left side of the header bar')
+def then_a_header_group1_will_be_displayed_on_the_left_side_of_the_header_bar(step, group1):
+    assert True, 'This step must be implemented'
