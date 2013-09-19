37a5f163ff7d861d7d7a6f6f3f1915fb5298890a
tushabe <tushabe@bitbucket.com>
2013-09-19 15:41:32 +0300

    Update feature tests for Show and Hide Marker Summary Info [#56679992]

--- features/ui.feature
+++ features/ui.feature
@@ -12,3 +12,14 @@ Scenario: Display page header
 Given That I am a regular user
 When I go to the home page
 Then A header 'Devtrac Global' will be displayed on the left side of the header bar
+
+Scenario: Show Marker Summary Info
+Given that I am a regular user
+When I  hover over the water point maker at 23.0000, 11.0000
+Then a summary of information about the marker is displayed
+
+Scenario: Hide Marker Summary Info
+Given that I am a regular user
+And the summary information about the water point marker at ... is displayed
+When I hover away from a marker
+Then the summary information is removed.
--- features/ui_steps.py
+++ features/ui_steps.py
@@ -20,3 +20,19 @@ def and_it_will_be_of_height_150px(step):
 @step(u'Then A header \'([^\']*)\' will be displayed on the left side of the header bar')
 def then_a_header_group1_will_be_displayed_on_the_left_side_of_the_header_bar(step, group1):
     assert True, 'This step must be implemented'
+
+@step(u'When I  hover over the water point maker at 23.0000, 11.0000')
+def when_i_hover_over_the_water_point_maker_at_23_0000_11_0000(step):
+    assert True, 'This step must be implemented'
+@step(u'Then a summary of information about the marker is displayed')
+def then_a_summary_of_information_about_the_marker_is_displayed(step):
+    assert True, 'This step must be implemented'
+@step(u'And the summary information about the water point marker at ... is displayed')
+def and_the_summary_information_about_the_water_point_marker_at_is_displayed(step):
+    assert True, 'This step must be implemented'
+@step(u'When I hover away from a marker')
+def when_i_hover_away_from_a_marker(step):
+    assert True, 'This step must be implemented'
+@step(u'Then the summary information is removed.')
+def then_the_summary_information_is_removed(step):
+    assert True, 'This step must be implemented'
