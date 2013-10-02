d98bb0529be53b826f4b1ca9f895c41ca24a855b
Timothy Cochran <timothy.cochran@gmail.com>
2013-10-02 20:02:12 +0300

    removing data models

--- lib/services.py
+++ lib/services.py
@@ -1,19 +1,9 @@
-from data_models import *
 import pymongo
 from pymongo import MongoClient
 import urllib
 import json
 
 from flask import Flask
-from flask.ext.mongoengine import MongoEngine
-
-class DistrictService(object):  
-
-    def find_by_name(self, name):
-        return District.objects(index_name=name.upper()).first()
-
-    def find_all(self):
-        return District.objects().exclude('geometry', 'unicef', 'subcounties').all().order_by("name")
 
 class AggregationService(object):
 
