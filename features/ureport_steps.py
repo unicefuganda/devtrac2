fdbb842afcd1b0c439cc48f1d0e8434f8405143e
kasmatt <kasmatt2000@msn.com>
2013-10-17 16:22:51 +0300

    Ureport feature [#56679202]

---
+++ features/ureport_steps.py
@@ -0,0 +1,17 @@
+from lettuce import *
+from page import *
+from nose.tools import *
+from time import *
+
+@step(u'And I select a ureport question "([^"]*)"')
+def and_i_select_a_ureport_question_group1(step, group1):
+    assert True, 'This step must be implemented'
+@step(u'Then the bottom panel will be displayed')
+def then_the_bottom_panel_will_be_displayed(step):
+    assert True, 'This step must be implemented'
+@step(u'And A badge indicating the highest reponse will be displayed for each district')
+def and_a_badge_indicating_the_highest_reponse_will_be_displayed_for_each_district(step):
+    assert True, 'This step must be implemented'
+@step(u'And 5 reponses for "([^"]*)" will be displayed on bottom panel')
+def and_5_reponses_for_group1_will_be_displayed_on_bottom_panel(step, group1):
+    assert True, 'This step must be implemented'
