c8525a466e14396a1c2ee03585071e957e9de30c
Timothy Cochran <timothy.cochran@gmail.com>
2013-10-18 07:21:58 +0300

    disabling flaky tests

--- features/terrain.py
+++ features/terrain.py
@@ -4,7 +4,7 @@ from splinter import Browser
 window_sizes = {"phantomjs": [1280, 800], "firefox": [1280, 880]}
 @before.all
 def setup_all():
-    browser = "firefox"
+    browser = "phantomjs"
     world.browser = Browser(browser)
     world.browser.driver.set_window_size(window_sizes[browser][0], window_sizes[browser][1])
 
