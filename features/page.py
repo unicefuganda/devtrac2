0c9c03b226fde3ac39a2c9960501a02f52e67de2
Timothy Cochran <timothy.cochran@gmail.com>
2013-10-22 20:33:05 +0300

    use basemap test urls in functional tests

--- features/page.py
+++ features/page.py
@@ -7,7 +7,7 @@ class Page:
         self.browser = browser
 
     def visit_national_dashboard(self):
-        self.browser.visit("%s/?test=true" % self.base_url)
+        self.browser.visit("%s/?basemap=test" % self.base_url)
 
     def visit_dashboard(self, location_name):
         locations = location_name.lower().split(", ")
@@ -24,7 +24,7 @@ class Page:
         if (len(locations) > 3):
             url += ("/%s" % locations[3]) 
 
-        self.browser.visit("%s/dashboard%s?test=true" % (self.base_url, url))
+        self.browser.visit("%s/dashboard%s?basemap=test" % (self.base_url, url))
 
     def breadcrumbs(self):
         crumbs = map(lambda crumb:crumb.text, self.browser.find_by_css("#location .breadcrumb li"))
