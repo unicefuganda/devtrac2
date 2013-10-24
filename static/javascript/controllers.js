6ffd99ba4625796a6cfb438456964dcffd1a0c95
Timothy Cochran <timothy.cochran@gmail.com>
2013-10-24 11:47:05 +0300

    don't show health center and water points by default, min height on detail panel

--- static/javascript/controllers.js
+++ static/javascript/controllers.js
@@ -3,7 +3,7 @@ angular.module("dashboard").controller("DashboardCtrl", function($rootScope, $ro
 
     $rootScope.location = new DT.Location($routeParams);
     if ($rootScope.filter == undefined)
-        $rootScope.filter = new DT.Filter({health_center: true, water_point: true, school: true});
+        $rootScope.filter = new DT.Filter({health_center: false, water_point: false, school: true});
 
 
 }).controller("IndicatorsCtrl", function($scope, $rootScope, heatmapService) {
