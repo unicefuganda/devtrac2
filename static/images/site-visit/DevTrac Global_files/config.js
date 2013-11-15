23768028b0b5f52048851395c46a2aac0e1ae181
Timothy Cochran <timothy.cochran@gmail.com>
2013-11-15 09:17:30 +0300

    removing tunjis extra files

--- static/images/site-visit/DevTrac Global_files/config.js
+++
@@ -1,6 +0,0 @@
-if (typeof DT == "undefined") 
-    DT = {};
-
-DT.piechart_colors = ["red", "blue", "yellow", "green", "lightblue", "orange", "salmon", "cyan", "magenta"];
-
-DT.markerColors = [ 'red', 'blue', 'green', 'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'goldenrod', 'greenyellow', 'hotpink', 'indianred', 'indigo', 'khaki', 'lavender', 'lightblue', 'lightcoral', 'lightgoldenrodyellow', 'lightslategray', 'lightsteelblue', 'lightyellow', 'magenta', 'maroon', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise' ];
--- static/images/site-visit/DevTrac Global_files/filters.js
+++
@@ -1,5 +0,0 @@
-angular.module("dashboard").filter('percent', function() {
-  return function(value) {
-    return (parseFloat(value) * 100).toFixed(0).toString() + "%"
-  };
-});
